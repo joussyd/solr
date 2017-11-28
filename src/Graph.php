b9ba59663742e667e82e12029d8f4c82baf8ec06
Joussyd Calupig <joussydmcalupig@gmail.com>
2017-11-28 22:06:13 +0800

    Initial commit

---
+++ src/Graph.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of the Compos Mentis Inc.
+ * PHP version 7+ (c) 2017 CMI
+ *
+ * Copyright and license information can be found at LICENSE
+ * distributed with this package.
+ *
+ * @category Class
+ * @package  Class
+ * @author   Joussyd Calupig <joussydmcalupig@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+namespace Lethia\Solr;
+
+/**
+ * Graph Class
+ *
+ * PHP version 7+
+ *
+ * @category Class
+ * @author   Joussyd Calupig <joussydmcalupig@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+class Search extends Factory
+{
+}
---
+++ src/Search.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the Compos Mentis Inc.
+ * PHP version 7+ (c) 2017 CMI
+ *
+ * Copyright and license information can be found at LICENSE
+ * distributed with this package.
+ *
+ * @category Class
+ * @package  Class
+ * @author   Joussyd Calupig <joussydmcalupig@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+namespace Lethia\Solr;
+use Lethia\Solr\Factory;
+
+
+/**
+ * Base Class
+ *
+ * PHP version 7+
+ *
+ * @category Class
+ * @author   Joussyd Calupig <joussydmcalupig@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+class Base 
+{
+    /* Constants
+    ---------------------------------------------*/
+    /* Protected Properties
+    ---------------------------------------------*/
+    /* Private Properties
+    ---------------------------------------------*/
+    /* Constructor
+    ---------------------------------------------*/
+    /* Public Methods
+    ---------------------------------------------*/
+    /* Protected Methods
+    ---------------------------------------------*/
+    /* Private Methods
+    ---------------------------------------------*/
+}
